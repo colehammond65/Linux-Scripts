4986b7b79342c9add6cef47f8f142d7301bfb8f6
Cole Hammond <cole.d.hammond@hotmail.co.nz>
2024-05-27 14:12:58 +1200

    Update restart_all_lxcs.sh

--- restart_all_lxcs.sh
+++ restart_all_lxcs.sh
@@ -6,7 +6,7 @@ containers=$(pct list | awk 'NR>1 {print $1}')
 # Restart each container
 for container in $containers; do
     echo "Restarting container $container..."
-    pct restart $container
+    pct reboot $container
 done
 
 echo "All LXC containers have been restarted."
